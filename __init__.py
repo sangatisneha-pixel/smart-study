1cdd28906644e732b8a484cd6832305e2b7f24af
sangatisneha-pixel <abird4154@gmail.com>
2026-07-17 22:01:36 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,49 +1,13 @@
-"""
-pypdf is a free and open-source pure-python PDF library capable of splitting,
-merging, cropping, and transforming the pages of PDF files. It can also add
-custom data, viewing options, and passwords to PDF files. pypdf can retrieve
-text and metadata from PDFs as well.
+from __future__ import annotations
 
-You can read the full docs at https://pypdf.readthedocs.io/.
-"""
+__version__ = "25.2"
 
-from ._crypt_providers import crypt_provider
-from ._doc_common import DocumentInformation
-from ._encryption import PasswordType
-from ._merger import PdfMerger
-from ._page import PageObject, Transformation, mult
-from ._reader import PdfReader
-from ._version import __version__
-from ._writer import ObjectDeletionFlag, PdfWriter
-from .constants import ImageType
-from .pagerange import PageRange, parse_filename_page_ranges
-from .papersizes import PaperSize
 
-try:
-    import PIL
+def main(args: list[str] | None = None) -> int:
+    """This is an internal API only meant for use by pip's own console scripts.
 
-    pil_version = PIL.__version__
-except ImportError:
-    pil_version = "none"
+    For additional details, see https://github.com/pypa/pip/issues/7498.
+    """
+    from pip._internal.utils.entrypoints import _wrapper
 
-_debug_versions = (
-    f"pypdf=={__version__}, crypt_provider={crypt_provider}, PIL={pil_version}"
-)
-
-__all__ = [
-    "__version__",
-    "_debug_versions",
-    "ImageType",
-    "mult",
-    "PageRange",
-    "PaperSize",
-    "DocumentInformation",
-    "ObjectDeletionFlag",
-    "parse_filename_page_ranges",
-    "PdfMerger",
-    "PdfReader",
-    "PdfWriter",
-    "Transformation",
-    "PageObject",
-    "PasswordType",
-]
+    return _wrapper(args)
